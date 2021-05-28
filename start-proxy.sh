1e4249bd2e827c4473e8be076f8cb8d36f42e750
Sami Salkosuo <samisalkosuo@users.noreply.github.com>
2021-05-28 12:37:02 +0300

    proxy preserve host

--- start-proxy.sh
+++ start-proxy.sh
@@ -1,13 +1,21 @@
 
 
-if [[ "$PROXY_PASS" == "" ]]; then
-  #set apache logs as links to system out and system error
-  echo "PROXY_PASS environment variable not set."
-  echo "Set PROXY_PASS to backend host."
+if [[ "$BACKEND_HOST" == "" ]]; then
+  echo "BACKEND_HOST environment variable not set."
+  echo "Set BACKEND_HOST to backend host."
+  echo "For example:"
+  echo "  BACKEND_HOST=server.internal.net"
   exit 1
-else
-   sed -i "s|PROXY_PASS|${PROXY_PASS}|g" /usr/local/apache2/conf/sites/reverse_proxy.conf
 fi
+
+if [[ "$BACKEND_PROTOCOL" == "" ]]; then
+  echo "BACKEND_PROTOCOL environment variable not set."
+  echo "Set BACKEND_PROTOCOL to 'http' or 'https'."
+  exit 1
+fi
+
+sed -i "s|BACKEND_HOST|${BACKEND_HOST}|g" /usr/local/apache2/conf/sites/reverse_proxy.conf
+sed -i "s|BACKEND_PROTOCOL|${BACKEND_PROTOCOL}|g" /usr/local/apache2/conf/sites/reverse_proxy.conf
 
 if [[ "$LOG" == "true" ]]; then
   #set apache logs as links to system out and system error
@@ -32,6 +40,5 @@ case $LOG_FORMAT in
 esac
 #set log format
 sed -i "s|LOG_FORMAT|${__log_format}|g" /usr/local/apache2/conf/httpd.conf
-
 
 exec httpd -D FOREGROUND
